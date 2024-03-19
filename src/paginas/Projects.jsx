98cea72de87ebd6df10be1a4ab06e66d070609dc
unknown <apraiz.mikel@gmail.com>
2024-03-19 19:44:08 +0100

    Sprint 20 entregable 1

---
+++ src/paginas/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import projects from "../data/projects.js"
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Proyectos</h1>
+      <p>{projects}</p>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/paginas/Resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import resume from "../data/resume.js"
+
+const Resume = () => {
+  return (
+    <div>
+        <h1>Resume</h1>
+      <p>{resume}</p>
+    </div>
+  );
+};
+
+export default Resume;
